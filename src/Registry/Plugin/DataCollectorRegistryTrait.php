505e326bd153c06ff86f06a8e01467849524f43b
Stephan Ude <ude@mediatis.de>
2023-01-11 20:58:54 +0100

    [BUGFIX] namespace fixes

--- src/Registry/Plugin/DataCollectorRegistryTrait.php
+++ src/Registry/Plugin/DataCollectorRegistryTrait.php
@@ -2,7 +2,7 @@
 
 namespace DigitalMarketingFramework\Collector\Core\Registry\Plugin;
 
-use DataCollector\Core\DataCollector\DataCollectorInterface;
+use DigitalMarketingFramework\Collector\Core\DataCollector\DataCollectorInterface;
 use DigitalMarketingFramework\Collector\Core\Model\Configuration\CollectorConfiguration;
 use DigitalMarketingFramework\Core\Model\Configuration\ConfigurationInterface;
 use DigitalMarketingFramework\Core\Registry\Plugin\PluginRegistryTrait;
@@ -20,7 +20,7 @@ trait DataCollectorRegistryTrait
     {
         $this->deletePlugin($keyword, DataCollectorInterface::class);
     }
-    
+
     public function getDataCollector(string $keyword, ConfigurationInterface $configuration): ?DataCollectorInterface
     {
         return $this->getPlugin($keyword, DataCollectorInterface::class, [CollectorConfiguration::convert($configuration)]);
