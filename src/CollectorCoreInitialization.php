528b763e80d6a55551c786ec521d64d8403b374c
Stephan Ude <ude@mediatis.de>
2023-08-09 19:49:16 +0200

    [TASK] configuration document version handling

---
+++ src/CollectorCoreInitialization.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DigitalMarketingFramework\Collector\Core;
+
+use DigitalMarketingFramework\Collector\Core\DataProcessor\ValueModifier\MaskedValueModifier;
+use DigitalMarketingFramework\Core\DataProcessor\ValueModifier\ValueModifierInterface;
+use DigitalMarketingFramework\Core\Initialization;
+use DigitalMarketingFramework\Core\Registry\RegistryDomain;
+
+class CollectorCoreInitialization extends Initialization
+{
+    const PLUGINS = [
+        RegistryDomain::CORE => [
+            ValueModifierInterface::class => [
+                MaskedValueModifier::class,
+            ],
+        ],
+    ];
+
+    protected const SCHEMA_MIGRATIONS = [];
+
+    public function __construct()
+    {
+        parent::__construct('collector-core', '1.0.0');
+    }
+}
